c4dd38192850c757433a10bb84b066891de6b2c7
Stefan Page <90534062+stefpage@users.noreply.github.com>
2023-08-04 17:55:15 -0400

    without fakerjs

--- packages/server/dist/src/db/seeding/seeds/initialSeed.js
+++ packages/server/dist/src/db/seeding/seeds/initialSeed.js
@@ -9,117 +9,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const faker_1 = require("@faker-js/faker");
-const dayjs = require("dayjs");
-const uuid_1 = require("uuid");
-const User_1 = require("../../../entity/User");
 const Listing_1 = require("../../../entity/Listing");
-const Message_1 = require("../../../entity/Message");
-const Booking_1 = require("../../../entity/Booking");
-const common_1 = require("@airbnb-clone/common");
 class InitialSeeder {
     run(dataSource, factoryManager) {
         return __awaiter(this, void 0, void 0, function* () {
             const listingsRepository = dataSource.getRepository(Listing_1.Listing);
-            const messagesRepository = dataSource.getRepository(Message_1.Message);
-            const bookingsRepository = dataSource.getRepository(Booking_1.Booking);
-            const userFactory = factoryManager.get(User_1.User);
             const listingFactory = factoryManager.get(Listing_1.Listing);
-            const messageFactory = factoryManager.get(Message_1.Message);
-            const bookingFactory = factoryManager.get(Booking_1.Booking);
-            const users = yield userFactory.saveMany(10);
             const listings = yield Promise.all(Array(100)
                 .fill("")
                 .map(() => __awaiter(this, void 0, void 0, function* () {
-                const made = yield listingFactory.make({
-                    user: faker_1.faker.helpers.arrayElement(users),
-                });
+                const made = yield listingFactory.make({});
                 return made;
             })));
             yield listingsRepository.save(listings);
-            listings.map((listing) => __awaiter(this, void 0, void 0, function* () {
-                let guestUser;
-                do {
-                    guestUser = faker_1.faker.helpers.arrayElement(users);
-                } while (guestUser.id === listing.userId);
-                const hostUser = users.find((user) => user.id === listing.userId);
-                const conversationId = (0, uuid_1.v4)();
-                const message = yield messageFactory.make({
-                    text: `Hello, I'm ${guestUser.firstName}`,
-                    fromHost: false,
-                    guestUser,
-                    userIdOfGuest: guestUser.id,
-                    hostUser,
-                    userIdOfHost: hostUser.id,
-                    listing,
-                    listingId: listing.id,
-                    conversationId,
-                    createdDate: new Date(dayjs(Date.now()).subtract(2, "day").format()),
-                });
-                yield messagesRepository.save(message);
-                const message2 = yield messageFactory.make({
-                    text: `Hey there! I'm ${hostUser.firstName}`,
-                    fromHost: true,
-                    guestUser,
-                    userIdOfGuest: guestUser.id,
-                    hostUser,
-                    userIdOfHost: hostUser.id,
-                    listing,
-                    listingId: listing.id,
-                    conversationId,
-                    createdDate: new Date(dayjs(Date.now()).subtract(1, "day").subtract(34, "minute").format()),
-                });
-                yield messagesRepository.save(message2);
-                const message3 = yield messageFactory.make({
-                    text: "Your place looks really nice!",
-                    fromHost: false,
-                    guestUser,
-                    userIdOfGuest: guestUser.id,
-                    hostUser,
-                    userIdOfHost: hostUser.id,
-                    listing,
-                    listingId: listing.id,
-                    conversationId,
-                    createdDate: new Date(dayjs(Date.now()).subtract(1, "day").subtract(17, "minute").format()),
-                });
-                yield messagesRepository.save(message3);
-                const start = dayjs(Date.now()).add(1, "month").format(common_1.dateFormat);
-                const end = dayjs(start)
-                    .add(Math.floor(Math.random() * 10) + 1, "day")
-                    .format(common_1.dateFormat);
-                const { serviceFee, taxes, total } = (0, common_1.calculateBookingCosts)(listing.price, (0, common_1.getDayDifference)(start, end));
-                const booking1 = yield bookingFactory.make({
-                    range: `[${start}, ${end}]`,
-                    guests: Math.floor(Math.random() * listing.guests) + 1,
-                    pricePerNight: listing.price,
-                    serviceFee,
-                    taxes,
-                    total,
-                    user: guestUser,
-                    userId: guestUser.id,
-                    listing,
-                    listingId: listing.id,
-                });
-                yield bookingsRepository.save(booking1);
-                const start2 = dayjs(start).add(1, "month").format(common_1.dateFormat);
-                const end2 = dayjs(start2)
-                    .add(Math.floor(Math.random() * 10) + 1, "day")
-                    .format(common_1.dateFormat);
-                const { serviceFee: serviceFee2, taxes: taxes2, total: total2, } = (0, common_1.calculateBookingCosts)(listing.price, (0, common_1.getDayDifference)(start, end));
-                const booking2 = yield bookingFactory.make({
-                    range: `[${start2}, ${end2}]`,
-                    guests: Math.floor(Math.random() * listing.guests) + 1,
-                    pricePerNight: listing.price,
-                    serviceFee: serviceFee2,
-                    taxes: taxes2,
-                    total: total2,
-                    user: guestUser,
-                    userId: guestUser.id,
-                    listing,
-                    listingId: listing.id,
-                });
-                yield bookingsRepository.save(booking2);
-            }));
         });
     }
 }
