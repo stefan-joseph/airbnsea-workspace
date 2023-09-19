1ac28ec2764675f0ec7e06d787e08b2ff6094946
Stefan Page <90534062+stefpage@users.noreply.github.com>
2023-09-19 11:50:17 -0400

    styled catch-all messages page

--- packages/web/src/modules/textPage/TextPage.tsx
+++ packages/web/src/modules/textPage/TextPage.tsx
@@ -1,9 +1,29 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { AuthPageContainer } from "../auth/components/AuthPageContainer";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { appSidePadding } from "../../constants/constants";
 
 export const TextPage = () => {
+  const navigate = useNavigate();
   const {
     state: { message },
   } = useLocation();
 
-  return <h2>{message}</h2>;
+  return (
+    <AuthPageContainer>
+      <Stack m={appSidePadding} alignItems="center">
+        <Typography variant="h4" component="h2" gutterBottom>
+          {message}.
+        </Typography>
+        <Box>
+          <Button
+            onClick={() => navigate("/login")}
+            sx={{ padding: 2, paddingLeft: 4, paddingRight: 4 }}
+          >
+            Return to login page
+          </Button>
+        </Box>
+      </Stack>
+    </AuthPageContainer>
+  );
 };
